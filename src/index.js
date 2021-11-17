9dbef6cae76ace29b4865448bc572c3727b5f561
Matt Casperson <mc.casperson@gmail.com>
2021-11-17 08:51:07 -0800

    refactor to use fetch() instead of an XHR wrapped in a promise

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import WeatherService from './weather-service.js'
+import WeatherService from './js/weather-service.js';
 
 function clearFields() {
   $('#location').val("");
@@ -11,17 +11,22 @@ function clearFields() {
   $('.showTemp').text("");
 }
 
+function getElements(response) {
+  if (response.main) {
+    $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
+    $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response.message}`);
+  }
+}
+
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     clearFields();
-    let promise = WeatherService.getWeather(city);
-    promise.then(function(response) {
-      const body = JSON.parse(response);
-      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error}`);
-    });
+    WeatherService.getWeather(city)
+      .then(function(response) {
+        getElements(response);
+      });
   });
 });
